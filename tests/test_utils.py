d24d179206944391349524906993f1c1e5f51cf0
moopings <mongkonchai4412@gmail.com>
2017-08-25 00:18:55 +0700

    add test utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,5 @@
 import unittest
-from datetime import datetime
+from urllib.parse import urlparse, urlunparse
 
 from crawler_py.utils import *
 
@@ -11,13 +11,21 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(url_with_http, 'http://example.com')
 
     def test_is_relative_path(self):
-        self.assertIsNone(True)
+        self.assertFalse(is_relative_path('http://example.com'))
+        self.assertTrue(is_relative_path('example.com'))
+        self.assertTrue(is_relative_path('../helloworld'))
 
     def test_split_url(self):
-        self.assertIsNone(True)
+        url = 'http://example.com/path?name=john#segment'
+        result = split_url(url)
+        self.assertEqual(result.hostname, 'example.com')
+        self.assertEqual(result.resource, '/path?name=john#segment')
 
     def test_join_modifier_url(self):
-        self.assertIsNone(True)
+        url = 'http://example.com/path?name=john#segment'
+        url_parse = urlparse(url)
+        joined_url = join_modifier_url(url_parse.path, url_parse)
+        self.assertEqual(joined_url, '/path?name=john#segment')
 
 
 if __name__ == '__main__':
