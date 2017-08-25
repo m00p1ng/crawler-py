f2c220fa8d82613c84c429b6b48af875bac04b70
moopings <mongkonchai4412@gmail.com>
2017-08-25 22:59:19 +0700

    refactor robots parser

--- crawler_py/analyzer/robots_parser.py
+++ crawler_py/analyzer/robots_parser.py
@@ -8,14 +8,16 @@ class RobotsParser:
     def __init__(self, hostname, content):
         self.hostname = hostname
         self.content = content
+        self.resources = []
 
     def extract_link(self):
         print_log("Extracting robots.txt")
         lines = self._split_lines()
         for i, line in enumerate(lines):
             if re.match(r'^User-agent: \*', line):
-                self._match_disallow_link(i + 1, lines)
+                self.resources = self._match_disallow_link(i + 1, lines)
                 break
+        return self.resources
 
     def _split_lines(self):
         lines = self.content.split('\n')
@@ -23,18 +25,23 @@ class RobotsParser:
         return lines
 
     def _match_disallow_link(self, i, lines):
-        print_log(f"Adding '{self.hostname}' disallow lists")
+        resources = []
         while i < len(lines) and not re.match(r'^User-agent', lines[i]):
             result = re.match(r'^Disallow: (.*)', lines[i])
             if result:
-                self._save_disallow_link(result.group(1))
+                resources.append(result.group(1))
             i += 1
-        print_log(f"'{self.hostname}' disallow lists were added\n")
+        return resources
 
-    def _save_disallow_link(self, resource):
-        data = {
-            "hostname": self.hostname,
-            "resource": resource,
-        }
-        if not db.disallow_links.find_one(data):
-            db.disallow_links.insert(data)
+    def save(self, resources):
+        print_log(f"Adding '{self.hostname}' disallow lists")
+
+        for resource in resources:
+            data = {
+                "hostname": self.hostname,
+                "resource": resource,
+            }
+            if not db.disallow_links.find_one(data):
+                db.disallow_links.insert_one(data)
+
+        print_log(f"'{self.hostname}' disallow lists were added\n")
